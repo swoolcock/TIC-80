786fca906eeb1d7557a497da6431c415fb1acbfc
Samah <samahnub@gmail.com>
2026-09-07 16:06:30 +0930

    Add stubs and implement mouse

--- src/api/angelscript.cpp
+++ src/api/angelscript.cpp
@@ -39,6 +39,11 @@ typedef std::string string;
         assert(_result >= 0); \
     }
 
+#define GET_TIC_CORE(CTX, CORE, MEM) \
+    asIScriptContext* CTX = asGetActiveContext(); \
+    tic_core* CORE = static_cast<tic_core*>(CTX->GetUserData()); \
+    tic_mem* MEM = (tic_mem*)CORE;
+
 typedef struct
 {
     asIScriptEngine* engine;
@@ -64,7 +69,6 @@ static asIScriptModule* asCompileModule(asIScriptEngine* engine, const char* cod
 }
 
 static void asMessageCallback(const asSMessageInfo *msg, void *param)
-// static void asMessageCallback(asIScriptGeneric* gen)
 {
     tic_core* core = static_cast<tic_core*>(param);
 
@@ -74,9 +78,9 @@ static void asMessageCallback(const asSMessageInfo *msg, void *param)
     else if( msg->type == asMSGTYPE_INFORMATION )
         type = "INFO";
 
-    char buffer[16384];
-    sprintf(buffer, "%s (%d, %d) : %s : %s", msg->section, msg->row, msg->col, type, msg->message);
-    core->data->error(core->data->data, buffer);
+    char buf[1024];
+    snprintf(buf, sizeof(buf), "%s (%d, %d) : %s : %s", msg->section, msg->row, msg->col, type, msg->message);
+    core->data->error(core->data->data, buf);
 }
 
 static string asToString(asIScriptGeneric* gen, asUINT arg)
@@ -137,35 +141,137 @@ static string asToString(asIScriptGeneric* gen, asUINT arg)
     }
 }
 
+static void as_peek(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_poke(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_peek1(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_poke1(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_peek2(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_poke2(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_peek4(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_poke4(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
 // static void as_cls(uint8 color)
 static void as_cls(asIScriptGeneric* gen)
 {
-    asIScriptContext* ctx = asGetActiveContext();
-    tic_core* core = static_cast<tic_core*>(ctx->GetUserData());
-    tic_mem* mem = (tic_mem*)core;
+    GET_TIC_CORE(ctx, core, mem);
 
     const u8 color = gen->GetArgByte(0);
     core->api.cls(mem, color);
 }
 
-// static bool as_btn(int button)
+static void as_pix(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_line(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_rect(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_rectb(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_circ(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_circb(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_elli(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_ellib(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_paint(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_tri(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_trib(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_ttri(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_clip(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_btnp(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
 static void as_btn(asIScriptGeneric* gen)
 {
-    asIScriptContext* ctx = asGetActiveContext();
-    tic_core* core = static_cast<tic_core*>(ctx->GetUserData());
-    tic_mem* mem = (tic_mem*)core;
+    GET_TIC_CORE(ctx, core, mem);
 
     const s32 button = (s32)gen->GetArgDWord(0);
     bool rv = core->api.btn(mem, button) != 0;
     *(bool*)gen->GetAddressOfReturnLocation() = rv;
 }
 
-// void spr(int id, int x, int y, int colorkey, int scale, uint8 flip, uint8 rotate, int w, int h)
 static void as_spr(asIScriptGeneric* gen)
 {
-    asIScriptContext* ctx = asGetActiveContext();
-    tic_core* core = static_cast<tic_core*>(ctx->GetUserData());
-    tic_mem* mem = (tic_mem*)core;
+    GET_TIC_CORE(ctx, core, mem);
 
     const s32 id = (s32)gen->GetArgDWord(0);
     const s32 x = (s32)gen->GetArgDWord(1);
@@ -198,12 +304,74 @@ static void as_spr(asIScriptGeneric* gen)
     core->api.spr(mem, id, x, y, w, h, colors, colors_count, scale, tic_flip(flip), tic_rotate(rotate));
 }
 
-// void print(const ?&in text, int x, int y, uint8 color, bool fixed, int scale, bool alt)
+static void as_mget(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_mset(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_map(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_music(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_sfx(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_vbank(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_sync(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_reset(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_key(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_keyp(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_memcpy(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_memset(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_font(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
 static void as_print(asIScriptGeneric* gen)
 {
-    asIScriptContext* ctx = asGetActiveContext();
-    tic_core* core = static_cast<tic_core*>(ctx->GetUserData());
-    tic_mem* mem = (tic_mem*)core;
+    GET_TIC_CORE(ctx, core, mem);
 
     string text = asToString(gen, 0);
     const s32 x = (s32)gen->GetArgDWord(1);
@@ -213,7 +381,77 @@ static void as_print(asIScriptGeneric* gen)
     const s32 scale = (s32)gen->GetArgDWord(5);
     const bool alt = *(bool*)gen->GetAddressOfArg(6);
 
-    core->api.print(mem, text.c_str(), x, y, color, fixed, scale, alt);
+    s32 width = core->api.print(mem, text.c_str(), x, y, color, fixed, scale, alt);
+    *(s32*)gen->GetAddressOfReturnLocation() = width;
+}
+
+static void as_trace(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_pmem(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_time(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_tstamp(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_exit(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_mouse(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+
+    s32* x = (s32*)gen->GetArgAddress(0);
+    s32* y = (s32*)gen->GetArgAddress(1);
+    bool* left = (bool*)gen->GetArgAddress(1);
+    bool* middle = (bool*)gen->GetArgAddress(1);
+    bool* right = (bool*)gen->GetArgAddress(1);
+    s16* scrollx = (s16*)gen->GetArgAddress(1);
+    s16* scrolly = (s16*)gen->GetArgAddress(1);
+
+    const tic80_mouse* mouse = &core->memory.ram->input.mouse;
+    tic_point pt = core->api.mouse(mem);
+
+    if (x) *x = pt.x;
+    if (y) *y = pt.y;
+    if (left) *left = (bool)mouse->left;
+    if (middle) *middle = (bool)mouse->middle;
+    if (right) *right = (bool)mouse->right;
+    if (scrollx) *scrollx = mouse->scrollx;
+    if (scrolly) *scrolly = mouse->scrolly;
+}
+
+static void as_fget(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_fset(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_fft(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
+}
+
+static void as_ffts(asIScriptGeneric* gen)
+{
+    GET_TIC_CORE(ctx, core, mem);
 }
 
 static void initAPI(tic_core* core)
@@ -224,11 +462,63 @@ static void initAPI(tic_core* core)
     r = vm->engine->SetMessageCallback(asFUNCTION(asMessageCallback), core, asCALL_CDECL);
     assert(r >= 0);
 
+    REGISTER_TIC(vm, as_peek, "uint8 peek(int address, int bits=8)");
+    REGISTER_TIC(vm, as_poke, "void poke(int address, uint8 value, int bits=8)");
+    REGISTER_TIC(vm, as_peek1, "uint8 peek1(int address)");
+    REGISTER_TIC(vm, as_poke1, "void poke1(int address, uint8 value)");
+    REGISTER_TIC(vm, as_peek2, "uint8 peek2(int address)");
+    REGISTER_TIC(vm, as_poke2, "void poke2(int address, uint8 value)");
+    REGISTER_TIC(vm, as_peek4, "uint8 peek4(int address)");
+    REGISTER_TIC(vm, as_poke4, "void poke4(int address, uint8 value)");
     REGISTER_TIC(vm, as_cls, "void cls(uint8)");
-    REGISTER_TIC(vm, as_btn, "bool btn(int)");
+    REGISTER_TIC(vm, as_pix, "void pix(int x, int y, uint8 color)");
+    REGISTER_TIC(vm, as_pix, "uint8 pix(int x, int y)");
+    REGISTER_TIC(vm, as_line, "void line(float x0, float y0, float x1, float y1, uint8 color)");
+    REGISTER_TIC(vm, as_rect, "void rect(int x, int y, int w, int h, uint8 color)");
+    REGISTER_TIC(vm, as_rectb, "void rectb(int x, int y, int w, int h, uint8 color)");
+    REGISTER_TIC(vm, as_circ, "void circ(int x, int y, int radius, uint8 color)");
+    REGISTER_TIC(vm, as_circb, "void circb(int x, int y, int radius, uint8 color)");
+    REGISTER_TIC(vm, as_elli, "void elli(int x, int y, int a, int b, uint8 color)");
+    REGISTER_TIC(vm, as_ellib, "void ellib(int x, int y, int a, int b, uint8 color)");
+    // REGISTER_TIC(vm, as_paint, "");
+    REGISTER_TIC(vm, as_tri, "void tri(float x1, float y1, float x2, float y2, float x3, float y3, uint8 color)");
+    REGISTER_TIC(vm, as_trib, "void trib(float x1, float y1, float x2, float y2, float x3, float y3, uint8 color)");
+    REGISTER_TIC(vm, as_ttri, "void ttri(float x1, float y1, float x2, float y2, float x3, float y3, float u1, float v1, float u2, float v2, float u3, float v3)");
+    REGISTER_TIC(vm, as_ttri, "void ttri(float x1, float y1, float x2, float y2, float x3, float y3, float u1, float v1, float u2, float v2, float u3, float v3, int texsrc)");
+    REGISTER_TIC(vm, as_ttri, "void ttri(float x1, float y1, float x2, float y2, float x3, float y3, float u1, float v1, float u2, float v2, float u3, float v3, int texsrc, uint8 chromakey, float z1=0, float z2=0, float z3=0)");
+    REGISTER_TIC(vm, as_ttri, "void ttri(float x1, float y1, float x2, float y2, float x3, float y3, float u1, float v1, float u2, float v2, float u3, float v3, int texsrc, const array<int>@ chromakey, float z1=0, float z2=0, float z3=0)");
+    REGISTER_TIC(vm, as_clip, "void clip()");
+    REGISTER_TIC(vm, as_clip, "void clip(int x, int y, int width, int height)");
+    REGISTER_TIC(vm, as_btnp, "bool btnp(int id, int hold=-1, int period=-1)");
+    REGISTER_TIC(vm, as_btn, "bool btn(int id)");
     REGISTER_TIC(vm, as_spr, "void spr(int id, int x, int y, int colorkey=-1, int scale=1, uint8 flip=0, uint8 rotate=0, int w=1, int h=1)");
     REGISTER_TIC(vm, as_spr, "void spr(int id, int x, int y, const array<int>@ colorkey, int scale=1, uint8 flip=0, uint8 rotate=0, int w=1, int h=1)");
-    REGISTER_TIC(vm, as_print, "void print(const ?&in value, int x=0, int y=0, uint8 color=15, bool fixed=false, int scale=1, bool smallfont=false)");
+    REGISTER_TIC(vm, as_mget, "uint8 mget(int x, int y)");
+    REGISTER_TIC(vm, as_mset, "void mset(int x, int y, uint8 tile_id)");
+    // REGISTER_TIC(vm, as_map, ""); // TODO: map requires remap callback
+    REGISTER_TIC(vm, as_music, "void music(int track=-1, int frame=-1, int row=-1, bool loop=true, bool sustain=false, int tempo=-1, int speed=-1)");
+    REGISTER_TIC(vm, as_sfx, "void sfx(int id, int note=-1, int duration=-1, int channel=0, int volume=15, int speed=0)");
+    REGISTER_TIC(vm, as_vbank, "int vbank(int bank)");
+    REGISTER_TIC(vm, as_vbank, "int vbank()");
+    REGISTER_TIC(vm, as_sync, "void sync(uint mask=0, int bank=0, bool tocart=false)");
+    REGISTER_TIC(vm, as_reset, "void reset()");
+    REGISTER_TIC(vm, as_key, "bool key(int code=-1)");
+    REGISTER_TIC(vm, as_keyp, "bool keyp(int code=-1, int hold=-1, int period=-1)");
+    REGISTER_TIC(vm, as_memcpy, "void memcpy(int dest, int source, int size)");
+    REGISTER_TIC(vm, as_memset, "void memset(int dest, uint8 value, int size)");
+    REGISTER_TIC(vm, as_font, "int font(const ?&in text, int x, int y, int chromakey, int char_width, int char_height, bool fixed=false, int scale=1, bool alt=false)");
+    REGISTER_TIC(vm, as_print, "int print(const ?&in text, int x=0, int y=0, uint8 color=15, bool fixed=false, int scale=1, bool smallfont=false)");
+    REGISTER_TIC(vm, as_trace, "void trace(const ?&in text, uint8 color=15)");
+    REGISTER_TIC(vm, as_pmem, "void pmem(int index, uint value)");
+    REGISTER_TIC(vm, as_pmem, "uint pmem(int index)");
+    REGISTER_TIC(vm, as_time, "double time()");
+    REGISTER_TIC(vm, as_tstamp, "int tstamp()");
+    REGISTER_TIC(vm, as_exit, "void exit()");
+    REGISTER_TIC(vm, as_mouse, "void mouse(int &out x, int &out y, bool &out left=void, bool &out middle=void, bool &out right=void, int &out scrollx=void, int &out scrolly=void)");
+    REGISTER_TIC(vm, as_fget, "bool fget(int sprite_id, uint8 flag)");
+    REGISTER_TIC(vm, as_fset, "void fset(int sprite_id, uint8 flag, bool value)");
+    // REGISTER_TIC(vm, as_fft, "");
+    // REGISTER_TIC(vm, as_ffts, "");
 }
 
 static void closeAngelScript(tic_mem* tic)
